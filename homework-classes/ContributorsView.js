78a3d5115a55fb2d1a0470047f23cc899cb5a52c
Dima <dimakaddah@gmail.com>
2020-02-07 05:38:18 +0100

    add homeworkclasses

--- homework-classes/ContributorsView.js
+++ homework-classes/ContributorsView.js
@@ -20,6 +20,45 @@
      */
     render(contributors) {
       // TODO: replace this comment and the console.log with your own code
+      this.container.innerHTML = '';
+      contributors.forEach(contributor => {
+        const contrContainer = document.querySelector(
+          '.contributors-container',
+        );
+        const contributorHolder = createAndAppend('div', contrContainer, {
+          class: 'contributorHolder',
+        });
+        // set the json data in to array
+        let arrContributor = [
+          contributor.avatar_url,
+          contributor.login,
+          contributor.contributions,
+        ];
+        //loop in to array length to set info
+        for (let i = 0; i < arrContributor.length; i++) {
+          if (i === 0) {
+            let ImgDiv = createAndAppend('div', contributorHolder, {
+              class: 'ImgDiv',
+            });
+            let img = createAndAppend('img', ImgDiv);
+            img.src = arrContributor[0];
+          } else if (i === 1) {
+            let loginName = createAndAppend('div', contributorHolder, {
+              class: 'loginName',
+            });
+            createAndAppend('a', loginName, {
+              text: arrContributor[1],
+              href: contributor.html_url,
+              target: '_blank',
+            });
+          } else if (i === 2) {
+            let buttonDiv = createAndAppend('div', contributorHolder, {
+              class: 'buttonDiv',
+            });
+            createAndAppend('button', buttonDiv, { text: arrContributor[2] });
+          }
+        }
+      });
       console.log('ContributorsView', contributors);
     }
   }
--- homework-classes/RepoView.js
+++ homework-classes/RepoView.js
@@ -20,6 +20,37 @@
      */
     render(repo) {
       // TODO: replace this comment and the console.log with your own code
+      this.container.innerHTML = '';
+      const repoContainer = document.querySelector('.repo-container');
+      //create table for repos
+      const table = createAndAppend('table', repoContainer);
+      table.classList.add('reposTable');
+      //make arry for new keys to show
+      const displayKeys = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+      //make array of keys from repo json object
+      const keysNeeded = [
+        repo.name,
+        repo.description,
+        repo.fork,
+        repo.updated_at,
+      ];
+      //loop to get new name for keys and set the value
+      for (let i = 0; i < displayKeys.length; i++) {
+        let tr = createAndAppend('tr', table);
+        //tr for new keys
+        createAndAppend('th', tr, { text: displayKeys[i] });
+        //make a tag for repo name
+        if (i === 0) {
+          createAndAppend('a', tr, {
+            text: repo.name,
+            href: repo.html_url,
+            target: '_blank',
+          });
+          //make td for rest of info 'from jeson data'
+        } else {
+          createAndAppend('td', tr, { text: keysNeeded[i] });
+        }
+      }
       console.log('RepoView', repo);
     }
   }
